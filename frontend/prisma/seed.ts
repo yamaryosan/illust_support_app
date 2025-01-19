517ef86bae31f5f2ae0f1f09dfbcc3c36cc0191d
yamaryosan <canda3267@gmail.com>
2025-01-19 12:51:17 +0900

    add: 中間テーブル作成、シーディング前にテーブルをクリア

--- frontend/prisma/seed.ts
+++ frontend/prisma/seed.ts
@@ -3,20 +3,50 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.category.create({
+    // シーディング前にテーブルをクリア
+    await prisma.tagImage.deleteMany();
+    await prisma.tipImage.deleteMany();
+    await prisma.tipTag.deleteMany();
+    await prisma.subCategoryImage.deleteMany();
+    await prisma.subCategoryTip.deleteMany();
+
+    await prisma.subCategory.deleteMany();
+    await prisma.category.deleteMany();
+    await prisma.tip.deleteMany();
+    await prisma.image.deleteMany();
+    await prisma.tag.deleteMany();
+
+    // テーブルを作成
+    const category = await prisma.category.create({
         data: { name: "Category 1" },
     });
-    await prisma.subCategory.create({
-        data: { name: "SubCategory 1" },
+    const subCategory = await prisma.subCategory.create({
+        data: { name: "SubCategory 1", categoryId: category.id },
     });
-    await prisma.tip.create({
+    const tip = await prisma.tip.create({
         data: { title: "Tip 1", content: "Tip 1" },
     });
-    await prisma.image.create({
+    const image = await prisma.image.create({
         data: { path: "test.jpg", description: "test" },
     });
-    await prisma.tag.create({
+    const tag = await prisma.tag.create({
         data: { name: "Tag 1" },
+    });
+    // 中間テーブル
+    await prisma.subCategoryImage.create({
+        data: { subCategoryId: subCategory.id, imageId: image.id },
+    });
+    await prisma.subCategoryTip.create({
+        data: { subCategoryId: subCategory.id, tipId: tip.id },
+    });
+    await prisma.tagImage.create({
+        data: { tagId: tag.id, imageId: image.id },
+    });
+    await prisma.tipImage.create({
+        data: { tipId: tip.id, imageId: image.id },
+    });
+    await prisma.tipTag.create({
+        data: { tipId: tip.id, tagId: tag.id },
     });
 }
 
